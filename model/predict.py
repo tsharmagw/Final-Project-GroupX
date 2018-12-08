16ddeef91e36c499b24a21fa7f40609b57c89beb
Tejasvi Sharma <tejasvisharma@gwu.edu>
2018-12-07 23:54:47 -0500

    predict.py added

--- model/predict.py
+++ model/predict.py
@@ -1,140 +1,118 @@
-import io
+#@author- Tejasvi Sharma
+'''The purpose of this is to take data from 
+'''
 
-from PIL import Image, ImageOps, ImageDraw, ImageFont
-import requests
-import boto3
-from torch.autograd import Variable
-import torchvision.models as models
-import torchvision.transforms as transforms
+#Loading required packages
+import numpy as np # We'll be storing our data as numpy arrays
+import os # For handling directories
+from PIL import Image , ImageOps, ImageDraw, ImageFont# For handling the images
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg # Plotting
+
+#importing torch packages
 import torch
 import torchvision
-import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
-import numpy as np
-import torch.nn as nn
 from torch.autograd import Variable
-import warnings
-warnings.filterwarnings("ignore")
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+import boto3
 
-
-input_size = 1 * 28 * 28
-hidden_size = 100
-num_classes = 10
-num_epochs = 50
-batch_size = 10
-learning_rate = 0.01
-
-
-
-# Random cat img taken from Google
-#IMG_URL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'
-s3_client = boto3.resource('s3',
-	    	aws_access_key_id='AKIAIAQS3KGWKQL4JJ7A',
-	    	aws_secret_access_key='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',
-	    	region_name="us-east-1")
-
-key,bucket=open("/tmp/message_data.txt", "r").readline().split(";")
-#key='sn.jpg'
-#bucket='flaskdataml2'
-download_path = '/tmp/{}'.format(key)
-s3_client.Bucket(bucket).download_file(key, download_path)
-#logger.info("Data downloaded from s3 FIle name is "+str(key) )
-IMG_URL_LOCAL = download_path
-# Class labels used when training VGG as json, courtesy of the 'Example code' link above.
-#LABELS_URL = 'https://s3.amazonaws.com/outcome-blog/imagenet/labels.json'
-
-# Let's get our class labels.
-#response = requests.get(LABELS_URL)  # Make an HTTP GET request and store the response.
-#labels = {int(key): value for key, value in response.json().items()}
-
-# Let's get the cat img.
-#response = requests.get(IMG_URL)
-#img = Image.open(io.BytesIO(response.content))  # Read bytes and store as an img.
-
-img=Image.open(IMG_URL_LOCAL)
-
-# Let's take a look at this cat!
-
-#img.show()
-
-img = torchvision.transforms.functional.to_grayscale(img,1)
-
-#img.show()
-
-#img =torch.sub(torch.FloatTensor(255)-Variable(img))
-#img.show()
-
-
-
-
-min_img_size = 28  # The min size, as noted in the PyTorch pretrained models doc, is 224 px.
-transform_pipeline = transforms.Compose([transforms.Resize(min_img_size),
-                                         transforms.ToTensor(),
-                                         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-
-img = transform_pipeline(img)
-
-img=img.resize_((1,28,28))
-
-print(img.size())
-
-# PyTorch pretrained models expect the Tensor dims to be (num input imgs, num color channels, height, width).
-# Currently however, we have (num color channels, height, width); let's fix this by inserting a new axis.
-#img = img.unsqueeze(0)  # Insert the new axis at index 0 i.e. in front of the other axes/dims.
-
-# Now that we have preprocessed our img, we need to convert it into a
-# Variable; PyTorch models expect inputs to be Variables. A PyTorch Variable is a
-# wrapper around a PyTorch Tensor.
-
-
-
-class Net(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)
+#This class to transform the data, so that it can be loaded to test Loader
+class DatasetProcessing(Dataset):
+    def __init__(self, data, target, transform=None): #used to initialise the class variables - transform, data, target
+        self.transform = transform
+        self.data = data.reshape((-1,120,320)).astype(np.uint8)[:,:,:,None]
+        self.target = torch.from_numpy(target).float() # needs to be in torch.LongTensor dtype
+    def __getitem__(self, index): #used to retrieve the X and y index value and return it
+        return self.transform(self.data[index]), self.target[index]
+    def __len__(self): #returns the length of the data
+        return len(list(self.data))
+ 
+#CNN module, this is network architecture of model
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.layer1=nn.Sequential(
+            nn.Conv2d(1,32,kernel_size=5,stride=2),
+            nn.BatchNorm2d(32), #undertand, BatchNorm2D, why we have used it
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2))
+        self.layer2=nn.Sequential(
+            nn.Conv2d(32,64,kernel_size=3,stride=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2))
+        self.layer3=nn.Sequential(
+            nn.Conv2d(64,64,kernel_size=3,stride=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2))
+        self.classifier = nn.Sequential(
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10)
+        )
 
     def forward(self, x):
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = out.view(out.size(0), -1)
+        out=self.classifier(out)
         return out
 
-model = Net(input_size, hidden_size, num_classes)
-model.load_state_dict(torch.load('/home/model/model_Q5_cpuSGD.pth'))
-#model.load_state_dict(torch.load('./model/model_Q5_cpuSGD.pth'))
 
-print(model)
+if __name__=="__main__":
+
+    #code to read message from queue needs to be added
+    class_dict={9:'palm',7:'02_l',8:'fist',4:'fist_moved',6:'thumb',2:'index_finger',0:'ok',3:'palm_moved',5:'09_c',1:'10_down'}
+    s3_client = boto3.resource('s3',
+            aws_access_key_id='AKIAIAQS3KGWKQL4JJ7A',
+            aws_secret_access_key='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',
+            region_name="us-east-1")
+
+    key,bucket=open("/tmp/message_data.txt", "r").readline().split(";")
+
+    download_path = '/tmp/{}'.format(key)
+    s3_client.Bucket(bucket).download_file(key, download_path)
+    IMG_URL_LOCAL = download_path
+
+    img_array = []
+    # Read in and convert to greyscale
+    img=Image.open(IMG_URL_LOCAL).convert('L')
+    img.show()
+
+    img = img.resize((320, 120))
+    arr = np.array(img)
+    img_array.append(arr)
+    img_array1 = np.array(img_array, dtype = 'float32')
+
+    model_test = CNN()
+    model_test.load_state_dict(torch.load('/home/model/model_trained_cpu.pth'))
 
 
+    transform = transforms.Compose([transforms.ToPILImage(), transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))])
+    dset_test = DatasetProcessing(img_array1,np.array([[1]]), transform)
+    test_loader = torch.utils.data.DataLoader(dset_test, batch_size=1,shuffle=False,num_workers=1) #meaning of num_workers
 
 
-images = Variable(img.view(-1,28 * 28))
+    for i,(images,label) in enumerate(test_loader):
+        images = Variable(images)
+        outputs = model_test(images)
+        _, predicted = torch.max(outputs.data, 1)
+        pred_class=class_dict[predicted.cpu().numpy()[0]]
 
-print(images.size())
+    #adding label to image
+    print(pred_class)
+    img=Image.open(IMG_URL_LOCAL)
+    bimg = ImageOps.expand(img, border=20)
+    draw = ImageDraw.Draw(bimg)
+    draw.text((320, 20),pred_class,fill=(255))
+    bimg.save('/tmp/sample.jpg')
+    bucket1='flaskdataml2output'
+    s3_client.Bucket(bucket1).upload_file('/tmp/sample.jpg',Key=key+"-"+pred_class+".jpg")
 
-outputs = model(images)
-print(outputs.size())
-_, predicted = torch.max(outputs.data, 1)
-
-print(predicted.size())
-
-print(predicted)
-
-#predicted = predicted.data.cpu().numpy().argmax()
-
-
-
-classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot')
-pred_class=classes[predicted]
-#adding label to image
-img=Image.open(IMG_URL_LOCAL)
-bimg = ImageOps.expand(img, border=20)
-draw = ImageDraw.Draw(bimg)
-draw.text((28, 20),pred_class,(255,255,255))
-bimg.save('/tmp/sample.jpg')
-bucket1='flaskdataml2output'
-s3_client.Bucket(bucket1).upload_file('/tmp/sample.jpg',Key='pred_class'+".jpg")
 
