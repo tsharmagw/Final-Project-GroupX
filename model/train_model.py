72627456d1a2b61d5fbaf414cc8687fc1fc37d64
iamDeepakA <deepakagarwal@gwu.edu>
2018-12-08 22:31:54 -0500

    train model file

--- model/train_model.py
+++ model/train_model.py
@@ -1,12 +1,14 @@
-#Loading required packages
-import numpy as np # For storing data as numpy arrays
+# Loading required packages
+import numpy as np  # For storing data as numpy arrays
 import os  # For handling directories
 from PIL import Image  # For handling the images
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg # Plotting
+import matplotlib.image as mpimg  # Plotting
 from sklearn.model_selection import train_test_split
+import matplotlib.backends.backend_pdf
+#%matplotlib inline
 
-#importing torch packages
+# importing torch packages
 import torch
 import torchvision
 from torch.autograd import Variable
@@ -25,7 +27,7 @@ class DatasetProcessing(Dataset):
 
     def __init__(self, data, target, transform=None):
         self.transform = transform
-        self.data = data.reshape((-1,120,320)).astype(np.uint8)[:,:,:,None]
+        self.data = data.reshape((-1, 120, 320)).astype(np.uint8)[:, :, :, None]
         self.target = torch.from_numpy(target).float()
 
     def __getitem__(self, index):
@@ -42,6 +44,7 @@ class CNN(nn.Module):
     and relu as transfer/activation function
     for fully connected layer again we have used relu activation function
     '''
+
     def __init__(self):
         super(CNN, self).__init__()
         self.layer1 = nn.Sequential(
@@ -74,60 +77,62 @@ class CNN(nn.Module):
         return out
 
 
-
 # -----------------------------------------------------------------------------------
 # Train the Model
 
 def train(model, device, train_loader, optimizer, criterion, epoch):
     model.train()
-    loss_list=[]
+    loss_list = []
     for i, (images, labels) in enumerate(train_loader):
-            images = images.to(device)
-            labels = labels.to(device)
-            labels=labels.long()
-            labels=labels.view(-1,len(labels))[0]
-            optimizer.zero_grad()
-            outputs = model(images)
-            outputs=outputs.float()
-            loss = criterion(outputs,labels)
-            loss.backward()
-            optimizer.step()
-            loss_list.append(loss.item())
-            if i % 100 == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, i * len(images), len(train_loader.dataset),
-                           100. * i / len(train_loader), loss.item()))
+        images = images.to(device)
+        labels = labels.to(device)
+        labels = labels.long()
+        labels = labels.view(-1, len(labels))[0]
+        optimizer.zero_grad()
+        outputs = model(images)
+        outputs = outputs.float()
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        loss_list.append(loss.item())
+        if i % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, i * len(images), len(train_loader.dataset),
+                       100. * i / len(train_loader), loss.item()))
     # Save the model checkpoint
     torch.save(model.state_dict(), './model/model_trained.pth')
 
-    plt.plot(loss_list)
-    plt.show()
+    return loss_list
+
+    # plt.plot(loss_list)
+    # plt.show()
 
 
 def validate(model, device, validate_loader, optimizer, criterion, epoch):
     model.eval()
-    loss_list=[]
-    for epoch in range(NUM_EPOCHS):
+    loss_list = []
+    with torch.no_grad():
         for i, (images, labels) in enumerate(validate_loader):
             images = images.to(device)
             labels = labels.to(device)
-            labels=labels.long()
-            labels=labels.view(-1,len(labels))[0]
-            optimizer.zero_grad()
+            labels = labels.long()
+            labels = labels.view(-1, len(labels))[0]
+            # optimizer.zero_grad()
             outputs = model(images)
-            outputs=outputs.float()
-            loss = criterion(outputs,labels)
-            loss.backward()
-            optimizer.step()
+            outputs = outputs.float()
+            loss = criterion(outputs, labels)
+            # loss_list.append(loss)
+            # loss.backward()
+            # optimizer.step()
             loss_list.append(loss.item())
-            if i % 100 == 0:
+            if i % 10 == 0:
                 print('Validation Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                     epoch, i * len(images), len(validate_loader.dataset),
                            100. * i / len(validate_loader), loss.item()))
 
-    plt.plot(loss_list)
-    plt.show()
-
+        return loss_list
+    # plt.plot(loss_list)
+    # plt.show()
 
 
 def test(model, device, test_loader):
@@ -147,7 +152,6 @@ def test(model, device, test_loader):
             correct += (predicted == labels).sum().item()
 
         print('Test Accuracy of the model on the 3000 test images: {} %'.format(100 * correct / total))
-
 
 
 def main():
@@ -210,42 +214,95 @@ def main():
     print(x_test.shape)
     print(y_test.shape)
 
-    # specify the batch size, number of epochs and learning rate
-    BATCH_SIZE = 64
-    NUM_EPOCHS = 10
-    LEARNING_RATE = 0.01
+    batch_size_list = [64]
 
+    for BATCH_SIZE in batch_size_list:
 
-    # create instance of model
-    model = CNN().to(device)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+        # specify the transformation
+        transform = transforms.Compose(
+            [transforms.ToPILImage(), transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))])
+        # perform the pre-processing on train data
+        data_train = DatasetProcessing(x_train, y_train, transform)
+        # load the train data
+        train_loader = torch.utils.data.DataLoader(data_train, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
 
-    # specify the transformation
-    transform = transforms.Compose(
-        [transforms.ToPILImage(), transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))])
-    # perform the pre-processing on train data
-    data_train = DatasetProcessing(x_train, y_train, transform)
-    # load the train data
-    train_loader = torch.utils.data.DataLoader(data_train, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+        # specify the transformation
+        transform = transforms.Compose(
+            [transforms.ToPILImage(), transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))])
+        data_validate = DatasetProcessing(x_validate, y_validate, transform)
+        validate_loader = torch.utils.data.DataLoader(data_validate, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
 
-    # specify the transformation
-    transform = transforms.Compose(
-        [transforms.ToPILImage(), transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))])
-    data_validate = DatasetProcessing(x_validate, y_validate, transform)
-    validate_loader = torch.utils.data.DataLoader(data_validate, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+        # specify the transformation
+        transform = transforms.Compose(
+            [transforms.ToPILImage(), transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))])
+        data_test = DatasetProcessing(x_test, y_test, transform)
+        test_loader = torch.utils.data.DataLoader(data_test, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
 
-    # specify the transformation
-    transform = transforms.Compose(
-        [transforms.ToPILImage(), transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))])
-    data_test = DatasetProcessing(x_test, y_test, transform)
-    test_loader = torch.utils.data.DataLoader(data_test, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+        # specify the number of epochs and learning rate
+        learning_rate_list = [0.01]
+        optimizer_functions_list = ['Adam']
 
-    for epoch in range(1, NUM_EPOCHS):
-        train(model, device, train_loader, optimizer, criterion, NUM_EPOCHS)
-        validate(model, device, validate_loader, optimizer, criterion, NUM_EPOCHS)
-        test(model, device, test_loader)
+        for LEARNING_RATE in learning_rate_list:
 
+            for OPTIMIZER in optimizer_functions_list:
+                # create instance of model
+                model = CNN().to(device)
+                criterion = nn.CrossEntropyLoss()
+
+                if (OPTIMIZER == 'SGD'):
+                    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
+                elif (OPTIMIZER == 'ASGD'):
+                    optimizer = torch.optim.ASGD(model.parameters(), lr=LEARNING_RATE)
+                elif (OPTIMIZER == 'Adam'):
+                    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+                elif (OPTIMIZER == 'Adagrad'):
+                    optimizer = torch.optim.Adagrad(model.parameters(), lr=LEARNING_RATE)
+                elif (OPTIMIZER == 'Adadelta'):
+                    optimizer = torch.optim.Adadelta(model.parameters(), lr=LEARNING_RATE)
+                elif (OPTIMIZER == 'RMSProp'):
+                    optimizer = torch.optim.RMSprop(model.parameters(), lr=LEARNING_RATE)
+                # optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+                number_epochs_list = [2]
+
+                for NUM_EPOCHS in number_epochs_list:
+                    training_loss = []
+                    validation_loss = []
+                    mean_training_loss = []
+                    mean_validation_loss = []
+
+                    for epoch in range(1, NUM_EPOCHS + 1):
+                        train_loss = train(model, device, train_loader, optimizer, criterion, epoch)
+                        val_loss = validate(model, device, validate_loader, optimizer, criterion, epoch)
+                        training_loss = training_loss + train_loss
+                        validation_loss = validation_loss + val_loss
+                        mean_training_loss = mean_training_loss + [np.mean(train_loss)]
+                        mean_validation_loss = mean_validation_loss + [np.mean(val_loss)]
+
+                        #         for i in validation_loss:
+                        #             if val_loss>i:
+                        #                 print(val_loss)
+                        test(model, device, test_loader)
+
+                    fig1 = plt.figure()
+                    plt.plot(training_loss)
+                    plt.plot(validation_loss)
+                    # plt.show()
+                    fig1.savefig("batchwise_training_validation_loss_" + str(BATCH_SIZE) + "_" + str(LEARNING_RATE) +
+                                 "_" + str(OPTIMIZER) + "_" + str(NUM_EPOCHS) + ".png")
+
+                    fig2 = plt.figure()
+                    plt.plot(mean_training_loss)
+                    plt.plot(mean_validation_loss)
+                    # plt.show()
+                    fig2.savefig(
+                        "epochwise_mean_training_validation_loss_" + str(BATCH_SIZE) + "_" + str(LEARNING_RATE) +
+                        "_" + str(OPTIMIZER) + "_" + str(NUM_EPOCHS) + ".png")
+
+                    pdf = matplotlib.backends.backend_pdf.PdfPages("output.pdf")
+                    for fig in range(1, plt.gcf().number + 1):  ## will open an empty extra figure
+                        pdf.savefig(fig)
+                    pdf.close()
 
 
 if __name__ == '__main__':
