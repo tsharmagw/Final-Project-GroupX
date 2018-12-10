35a861d222893ad59c6c34733fc00ee9c35ddb9b
iamDeepakA <deepakagarwal@gwu.edu>
2018-12-10 01:03:48 -0500

    train model file

--- model/train_model.py
+++ model/train_model.py
@@ -1,13 +1,16 @@
-#%%-----------------------------------------------------------------------
+# %%-----------------------------------------------------------------------
 # Loading required packages
-import os  # For handling directories
+import os, csv  # For handling directories
 import matplotlib
+
 matplotlib.use('Agg')
 import matplotlib.backends.backend_pdf
 import matplotlib.pyplot as plt
 import numpy as np  # For storing data as numpy arrays
-
-#%%-----------------------------------------------------------------------
+import pandas as pd
+import timeit
+from pycm import *
+# %%-----------------------------------------------------------------------
 
 # importing torch packages
 import torch
@@ -16,11 +19,15 @@ import torchvision.transforms as transforms
 from PIL import Image  # For handling the images
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset
+
+
 # %matplotlib inline
 
 
 # %%-----------------------------------------------------------------------
-#os.system("wget https://s3.amazonaws.com/dataml2/leapGestRecog.rar")
+# os.system("wget https://s3.amazonaws.com/dataml2/leapGestRecog.tar.gz")
+# os.system("mkdir data")
+# os.system(tar -xf  leapGestRecog.tar.gz -C ~/MachineLearning2_Project/data
 # specify the class for preprocessing the data
 class DatasetProcessing(Dataset):
     """
@@ -115,7 +122,7 @@ def train(model, device, train_loader, optimizer, criterion, epoch):
 
 # %%-----------------------------------------------------------------------
 
-def validate(model, device, validate_loader, optimizer, criterion, epoch):
+def validate(model, device, validate_loader, criterion, epoch):
     model.eval()
     loss_list = []
     with torch.no_grad():
@@ -138,23 +145,66 @@ def validate(model, device, validate_loader, optimizer, criterion, epoch):
 
 # %%-----------------------------------------------------------------------
 
-def test(model, device, test_loader):
+def test(model, device, test_loader, criterion, epoch):
     # Test the model
     model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
+    loss_list = []
+    all_predicted = []
+    all_labels = []
+    classes = ('10_down', '04_fist_moved', '01_palm', '05_thumb', '02_l', '09_c', '08_palm_moved', '07_ok', '03_fist', '06_index')
     with torch.no_grad():
         correct = 0
         total = 0
-        for images, labels in test_loader:
+        for i, (images, labels) in enumerate(test_loader):
             images = images.to(device)
             labels = labels.to(device)
+            for item in labels.cpu().numpy():
+                all_labels.append(item)
             labels = labels.long()
             labels = labels.view(-1, len(labels))[0]
             outputs = model(images)
+            outputs = outputs.float()
+            loss = criterion(outputs, labels)
+            loss_list.append(loss.item())
+            if i % 10 == 0:
+                print('Validation Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, i * len(images), len(test_loader.dataset),
+                           100. * i / len(test_loader), loss.item()))
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
+            for item in predicted.cpu().numpy():
+            	all_predicted.append(item)
+
 
         print('Test Accuracy of the model on the 3000 test images: {} %'.format(100 * correct / total))
+        accuracy = 100 * correct / total
+	
+	
+        all_labels_array = np.array(all_labels).reshape(-1,)
+        all_predicted_array = np.array(all_predicted).reshape(-1,)
+
+        my_dict = dict(list(enumerate(classes)))
+
+        print(all_labels_array)
+        print(all_predicted_array)
+        print("My dict=",my_dict)
+        all_labels_vect = np.vectorize(my_dict.get)(all_labels_array)
+        print(all_labels_vect)
+        all_predicted_vect = np.vectorize(my_dict.get)(all_predicted_array)
+
+        # Create CM From Data
+        cm = ConfusionMatrix(predict_vector=all_predicted_vect, actual_vector=all_labels_vect)
+
+        # Create CM From Data
+        cm1 = ConfusionMatrix(actual_vector=all_labels_array, predict_vector=all_predicted_array)
+
+        # print(cm.F1)
+        # print(cm1)
+
+        # print(type(cm.F1))
+        # print(type(cm1))
+    return accuracy, loss_list, cm
 
 
 # %%-----------------------------------------------------------------------
@@ -174,6 +224,8 @@ def main():
             reverselookup[count] = j
             count = count + 1
     print(lookup)
+
+    classes = (lookup.keys())
 
     x_data = []
     label_data = []
@@ -219,7 +271,10 @@ def main():
     print(x_test.shape)
     print(y_test.shape)
 
-    batch_size_list = [128,64,32]
+    batch_size_list = [64]
+
+    results = {}
+    resultsDF = []
 
     for BATCH_SIZE in batch_size_list:
 
@@ -244,7 +299,7 @@ def main():
         test_loader = torch.utils.data.DataLoader(data_test, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
 
         # specify the number of epochs and learning rate
-        learning_rate_list = [0.1, 0.01, 0.001]
+        learning_rate_list = [0.001]
         optimizer_functions_list = ['Adam', 'Adadelta']
 
         for LEARNING_RATE in learning_rate_list:
@@ -267,7 +322,7 @@ def main():
                 elif OPTIMIZER == 'RMSProp':
                     optimizer = torch.optim.RMSprop(model.parameters(), lr=LEARNING_RATE)
 
-                number_epochs_list = [2, 5, 10]
+                number_epochs_list = [2]
 
                 for NUM_EPOCHS in number_epochs_list:
                     training_loss = []
@@ -276,13 +331,15 @@ def main():
                     mean_validation_loss = []
 
                     for epoch in range(1, NUM_EPOCHS + 1):
+                        start = timeit.default_timer()
                         train_loss = train(model, device, train_loader, optimizer, criterion, epoch)
-                        val_loss = validate(model, device, validate_loader, optimizer, criterion, epoch)
+                        stop = timeit.default_timer()
+                        val_loss = validate(model, device, validate_loader, criterion, epoch)
                         training_loss = training_loss + train_loss
                         validation_loss = validation_loss + val_loss
                         mean_training_loss = mean_training_loss + [np.mean(train_loss)]
                         mean_validation_loss = mean_validation_loss + [np.mean(val_loss)]
-                        test(model, device, test_loader)
+                        accuracy, testing_loss, cm = test(model, device, test_loader, criterion, epoch)
 
                     fig1 = plt.figure(figsize=(12, 8))
                     plt.plot(training_loss)
@@ -311,12 +368,67 @@ def main():
                         "epochwise_mean_training_validation_loss_" + str(BATCH_SIZE) + "_" + str(LEARNING_RATE) +
                         "_" + str(OPTIMIZER) + "_" + str(NUM_EPOCHS) + ".png")
 
+                    fig3 = plt.figure(figsize=(12, 8))
+                    plt.plot(training_loss)
+                    plt.xlabel("batch samples", fontsize=20)
+                    plt.ylabel("loss", fontsize=20)
+                    plt.legend(['testing loss'], loc='upper right', fontsize=20)
+                    plt.title("testing loss for batch size=" + str(BATCH_SIZE)
+                              + ", lr=" + str(LEARNING_RATE) +
+                              ", optimizer=" + str(OPTIMIZER) + ", num_epochs=" + str(NUM_EPOCHS), fontsize=15)
+                    # plt.show()
+                    fig3.savefig("testing_loss_" + str(BATCH_SIZE) + "_" + str(LEARNING_RATE) +
+                                 "_" + str(OPTIMIZER) + "_" + str(NUM_EPOCHS) + ".png")
+
+                    # Create CM From Data
+                    # class_labels = cm.table
+                    # cm_MAT = np.zeros((10, 10))
+                    #
+                    # print("class_labels",class_labels)
+                    # print(len(class_labels))
+                    #
+                    # fig4 = plt.figure(figsize=(12, 8))
+                    # for i in range(10):
+                    #     for j in range(10):
+                    #         cm_MAT[i, j] = class_labels[i][j]
+                    # plt.imshow(cm_MAT, interpolation='nearest', cmap=plt.cm.Greys)
+                    # plt.xticks(np.arange(10), classes , rotation=45)
+                    # plt.yticks(np.arange(10), classes)
+                    # plt.ylabel('True Label', fontsize=20)
+                    # plt.xlabel('Predicted  Label', fontsize=20)
+                    # plt.title("confusion matrix", fontsize=20)
+                    # fig4.savefig("confusion_matrix_" + str(BATCH_SIZE) + "_" + str(LEARNING_RATE) +
+                    #              "_" + str(OPTIMIZER) + "_" + str(NUM_EPOCHS) + ".png")
+
+
+
+                    results[(BATCH_SIZE, LEARNING_RATE, OPTIMIZER, NUM_EPOCHS)] = (round(stop - start, 2), round(accuracy, 2))
+
+                    print(results)
+
                     pdf = matplotlib.backends.backend_pdf.PdfPages("output.pdf")
                     for fig in range(1, plt.gcf().number + 1):  ## will open an empty extra figure
                         pdf.savefig(fig)
                     pdf.close()
 
+                    # with open('dict.csv', 'w') as csv_file:
+                    #     writer = csv.writer(csv_file)
+                    #     for key, value in results.items():
+                    #         writer.writerow([key, value])
 
+                df = pd.DataFrame(list(results.items()))
+                df1 = pd.DataFrame(df.iloc[:, 0].tolist(), columns=['batch_size', 'learning_rate', 'optimizer_method', 'num_epochs'])
+                df2 = pd.DataFrame(df.iloc[:, 1].tolist(), columns=['time', 'accuracy'])
+                df3 = pd.concat([df1, df2], axis=1)
+
+                df3.to_csv("df3.csv")
+
+                resultsDF.append(df3)
+
+
+    df_results = pd.concat(resultsDF)
+    df_results.drop_duplicates(keep=False)
+    df_results.to_csv("results1.csv")
 # %%-----------------------------------------------------------------------
 
 if __name__ == '__main__':
